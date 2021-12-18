18025d35d2ae016ad9bb6b72454b127e80c27409
Jeff-Aug <dentyn_nyt@outlook.com>
2021-12-18 12:26:38 -0300

    Exibir valores

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const transactionUL = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+
+
 const dummyTransctions = [
-    { id: 1, name: 'Nolo de Brigadeiro', amount: -20 },
+    { id: 1, name: 'Bolo de Brigadeiro', amount: -20 },
     { id: 2, name: 'Salario', amount: 300 },
     { id: 3, name: 'Torta de frango', amount: -10 },
     { id: 4, name: 'Violao', amount: 150 },
@@ -24,4 +29,35 @@ const addTransitionIntoDOM = transaction => {
 
 }
 
-addTransitionIntoDOM(dummyTransctions[0])
+const updateBalanceValues = () =>{
+    const transationAmounts = dummyTransctions.map(transaction => transaction.amount)
+    const total = transationAmounts.reduce((accumulator, transaction)=> accumulator + transaction,0).toFixed(2)
+    const income = transationAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator,value)=> accumulator + value, 0)
+        .toFixed(2)
+    const expense = Math.abs(transationAmounts
+        .filter(value => value < 0 )
+        .reduce((accumulator,value) => accumulator + value)
+        .toFixed(2) 
+        )
+    console.log(expense)
+
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
+}
+
+const init = () =>{
+    dummyTransctions.forEach(addTransitionIntoDOM)
+
+}
+
+init()
+updateBalanceValues()
+
+
+
+
+
+
